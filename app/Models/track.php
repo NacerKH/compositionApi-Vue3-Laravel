912c9f8892c60fb6f90607eac2eebd94a806376d
khalifa <mednaceur199530@gmail.com>
2022-04-15 02:38:18 +0100

    build css & castsing boolean field &fillable

--- app/Models/track.php
+++ app/Models/track.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Track extends Model
 {
     use HasFactory;
+    protected $casts =['is_favourite'=> 'boolean'];
+    protected $fillable= ['title', 'description', 'image','audio','is_favourite'];
+
 }
