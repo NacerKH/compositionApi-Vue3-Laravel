ae157dd3e42162e65d7f8a4ff23753e9043748c1
khalifa <mednaceur199530@gmail.com>
2022-05-09 23:41:59 +0100

    eturns the view for the broadcaster. We pass a 'type': broadcaster and the user's ID into the view to help identify who the user is

--- app/Http/Controllers/WebrtcStreamingController.php
+++ app/Http/Controllers/WebrtcStreamingController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\StreamAnswer;
+use App\Events\StreamOffer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WebrtcStreamingController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard', ['type' => 'broadcaster', 'id' => Auth::id()]);
+    }
+
+
 }
